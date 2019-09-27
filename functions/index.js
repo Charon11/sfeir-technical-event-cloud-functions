2ee776cf92937c81abcf26692c044a629af22d6b
Renaud Chardin <chardin.r@sfeir.com>
2019-09-27 17:31:24 +0200

    Add send mail

--- functions/index.js
+++ functions/index.js
@@ -1,11 +1,12 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 
-admin.initializeApp();
+admin.initializeApp(functions.config().firebase);
 
 exports.userCreated = functions.auth.user().onCreate((user) => {
     return admin.firestore().collection('users').doc(user.uid).set(
         {
+            uid: user.uid,
             email: user.email,
             emailVerified: user.emailVerified,
             displayName: user.displayName,
@@ -15,10 +16,77 @@ exports.userCreated = functions.auth.user().onCreate((user) => {
     ).then()
 });
 
+exports.grantAdminAccess = functions.pubsub.topic('grant-admin').onPublish(message => {
+    const userId = message.attributes['uid']
+    return admin.auth().setCustomUserClaims(userId, { admin: true }).then();
+})
+
 exports.publishSubjectEvent = functions.pubsub.topic('subject-events').onPublish(message => {
     const data = message.data ? Buffer.from(message.data, 'base64').toString() : '{"result":"no data"}';
     const event = JSON.parse(data)
     return admin.firestore().collection('subject-events').doc(event.id).set(event).then();
+})
+
+exports.eventAccepted = functions.pubsub.topic('accepted-subject-events').onPublish(message => {
+    const data = message.data ? Buffer.from(message.data, 'base64').toString() : '{"result":"no data"}';
+    const event = JSON.parse(data)
+    console.log(event);
+    return admin.firestore().collection('users').doc(event.userId).get().then(user => {
+        const email = user.data().email
+        return admin.firestore().collection('subject').doc(event.entityId).get().then(doc => {
+            return admin.firestore().collection('mail').doc().set(
+                {
+                    to : [email], 
+                    cc : ['chardin.r@sfeir.com, bodeving.l@sfeir.com, homberg.g@sfeir.com'],
+                    template : {
+                        name : 'event-accepted',
+                        data : {
+                            title : doc.data().title,
+                            date : doc.data().scheduleDate
+                        }
+                    }
+                }
+            ).then();
+
+        })
+    })
+})
+
+exports.eventCreated = functions.pubsub.topic('created-subject-events').onPublish(message => {
+    const data = message.data ? Buffer.from(message.data, 'base64').toString() : '{"result":"no data"}';
+    console.log(data);
+    const creatEvent = JSON.parse(data)
+    return admin.firestore().collection('users').doc(creatEvent.userId).get().then(user => {
+        const subject = {
+            id: creatEvent.entityId,
+            title: creatEvent.title,
+            status: creatEvent.status,
+            creationDate: new Date(creatEvent._ts),
+            schedules: creatEvent.schedules ? creatEvent.schedules : [],
+            record: creatEvent.record,
+            subjectType: creatEvent.subjectType
+        }
+        if (creatEvent.description) {
+            subject.description = creatEvent.description
+        }
+        subject.createdBy = user.data()
+        console.log(subject)
+        return admin.firestore().collection('mail').doc().set(
+            {
+                to : ['chardin.r@sfeir.com, bodeving.l@sfeir.com, homberg.g@sfeir.com'],
+                template : {
+                    name : 'event-created',
+                    data : {
+                        username : user.data().displayName,
+                        title : subject.title,
+                        type: subject.subjectType,
+                        description: subject.description ? subject.description : 'No description'
+                    }
+                }
+            }
+        ).then();
+    })
+
 })
 
 exports.publishSubjectCreatedEvent = functions.pubsub.topic('created-subject-events').onPublish(message => {
@@ -26,12 +94,13 @@ exports.publishSubjectCreatedEvent = functions.pubsub.topic('created-subject-eve
     console.log(data);
     const creatEvent = JSON.parse(data)
     return admin.firestore().collection('users').doc(creatEvent.userId).get().then(user => {
-        const subject = { 
+        const subject = {
             id: creatEvent.entityId,
             title: creatEvent.title,
             status: creatEvent.status,
             creationDate: new Date(creatEvent._ts),
-            schedules: creatEvent.schedules,
+            schedules: creatEvent.schedules ? creatEvent.schedules : [],
+            record: creatEvent.record,
             subjectType: creatEvent.subjectType
         }
         if (creatEvent.description) {
@@ -47,17 +116,19 @@ exports.publishSubjectCreatedEvent = functions.pubsub.topic('created-subject-eve
 exports.publishSubjectDeletedEvent = functions.pubsub.topic('deleted-subject-events').onPublish(message => {
     const data = message.data ? Buffer.from(message.data, 'base64').toString() : '{"result":"no data"}';
     const event = JSON.parse(data)
-    return admin.firestore().collection('users').doc(event.userId).get().then(user => {
-        return admin.firestore().collection('subject').doc(event.entityId).update({ status: event.status, updatedDate: new Date(event._ts), deletedBy: user.data() }).then();
-    })
-
+    return admin.firestore().collection('subject').doc(event.entityId).delete().then();
 })
 
 exports.publishSubjectAcceptedEvent = functions.pubsub.topic('accepted-subject-events').onPublish(message => {
     const data = message.data ? Buffer.from(message.data, 'base64').toString() : '{"result":"no data"}';
     const event = JSON.parse(data)
+    console.log(event);
     return admin.firestore().collection('users').doc(event.userId).get().then(user => {
-        return admin.firestore().collection('subject').doc(event.entityId).update({ status: event.status, acceptedBy: user.data(), scheduleDate: event.scheduleDate, updatedDate: new Date(event._ts) }).then();
+        const toUpdate = { status: event.status, acceptedBy: user.data(), scheduleDate: event.scheduleDate, updatedDate: new Date(event._ts) }
+        if (event.link) {
+            toUpdate.link = event.link
+        }
+        return admin.firestore().collection('subject').doc(event.entityId).update(toUpdate).then();
     })
 })
 
@@ -74,6 +145,14 @@ exports.publishSubjectDescriptionChangedEvent = functions.pubsub.topic('descript
     const event = JSON.parse(data)
     return admin.firestore().collection('users').doc(event.userId).get().then(user => {
         return admin.firestore().collection('subject').doc(event.entityId).update({ description: event.description, updatedBy: user.data(), updatedDate: new Date(event._ts) }).then();
+    })
+})
+
+exports.publishSubjectRecordAuthChangedEvent = functions.pubsub.topic('record-auth-changed-subject-events').onPublish(message => {
+    const data = message.data ? Buffer.from(message.data, 'base64').toString() : '{"result":"no data"}';
+    const event = JSON.parse(data)
+    return admin.firestore().collection('users').doc(event.userId).get().then(user => {
+        return admin.firestore().collection('subject').doc(event.entityId).update({ record: event.record, updatedBy: user.data(), updatedDate: new Date(event._ts) }).then();
     })
 })
 
@@ -97,6 +176,6 @@ exports.publishSubjectTypeChangedEvent = functions.pubsub.topic('type-changed-su
     const data = message.data ? Buffer.from(message.data, 'base64').toString() : '{"result":"no data"}';
     const event = JSON.parse(data)
     return admin.firestore().collection('users').doc(event.userId).get().then(user => {
-        return admin.firestore().collection('subject').doc(event.entityId).update({subjectType: event.subjectType , updatedBy: user.data(), updatedDate: new Date(event._ts) }).then();
+        return admin.firestore().collection('subject').doc(event.entityId).update({ subjectType: event.subjectType, updatedBy: user.data(), updatedDate: new Date(event._ts) }).then();
     })
 })
